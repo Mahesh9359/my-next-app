8342365d5d28e83095205ecca7505671cbd41a08
Mahesh9359 <maheshjadhav9359@gmail.com>
2025-07-26 22:31:18 +0530

    Error solved for deplyement

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -111,7 +111,7 @@ const Login: React.FC = () => {
                 <button
                   type="button"
                   onClick={() => signIn("google")}
-                  className="flex items-center gap-3 px-5 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50"
+                  className="flex items-center gap-3 px-5 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white cursor-pointer hover:bg-gray-50"
                 >
                   <Image
                     src={googleIcon}
@@ -124,7 +124,7 @@ const Login: React.FC = () => {
                 <button
                   type="button"
                   onClick={() => signIn("github")}
-                  className="flex items-center gap-3 px-5 py-2 border-[#44444] rounded-md shadow-sm text-sm font-medium text-white bg-[#444444] hover:bg-[#4e4e4e]"
+                  className="flex items-center gap-3 px-5 py-2 border-[#44444] rounded-md shadow-sm text-sm font-medium text-white bg-[#444444] cursor-pointer hover:bg-[#4e4e4e]"
                 >
                   <Image
                     src={githubIcon}
